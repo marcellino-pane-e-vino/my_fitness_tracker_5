d81b556917d177b1c87d57dc6a79c482de6c5ad2
mcentonze1 <mattia.centonze@iit.it>
2024-08-02 22:57:13 +0200

    0.0.1

---
+++ app/src/main/java/com/example/my_fitness_tracker_5/AddGoalActivity.java
@@ -0,0 +1,13 @@
+package com.example.my_fitness_tracker_5;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddGoalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_goal);
+    }
+}
